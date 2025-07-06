844ea247908f231ddd903d0990791f7ca7bcefa6
webxev-u <bannydc3@gmail.com>
2025-07-06 01:52:01 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/',
+  root: '.',
+  publicDir: 'public',
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'public/index.html')
+    }
+  }
 });
